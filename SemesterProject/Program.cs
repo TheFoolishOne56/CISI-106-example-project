c7418a9f2d93fc4d33f88f51a4777278b5b34fae
Aiden Whitacre <whitacrea@cougars.sf.edu>
2025-01-28 17:28:45 -0500

    svg builder working

--- SemesterProject/Program.cs
+++ SemesterProject/Program.cs
@@ -4,8 +4,11 @@ public static class Program
 {
     public static void Main()
     {
-        var svg = new SvgBuilder(500, 500).Build();
-
+        var svg = new SvgBuilder(500, 500)
+            .AddRectangle(100, 50, 150, 200, "#FF0000")
+            .AddRectangle(125, 400, 60, 30, "#00ff00")
+            .AddRectangle(350, 400, 60, 30, "#00ff00")
+            .Build();
         Console.Write("Absolute path to save SVG at: ");
         var path = Console.ReadLine() ?? "";
         using var streamWriter = new StreamWriter(path);
--- SemesterProject/SvgBuilder.cs
+++ SemesterProject/SvgBuilder.cs
@@ -2,9 +2,11 @@ namespace SemesterProject;
 
 public class SvgBuilder
 {
-    private const string SVG_HEADER_TEMPLATE = "<svg width=\"{0}\" height=\"{1}\" xmlns=\"http://www.w3.org/2000/svg\">";
+    private const string SVG_HEADER_TEMPLATE =
+        "<svg width=\"{0}\" height=\"{1}\" xmlns=\"http://www.w3.org/2000/svg\">";
     private const string SVG_FOOTER = "</svg>";
-    private const string RECT_TEMPLATE = "<rect x=\"{0}\" y=\"{1}\" width=\"{2}\" height=\"{3}\" style=\"fill:{4}\" />";
+    private const string RECT_TEMPLATE =
+        "<rect x=\"{0}\" y=\"{1}\" width=\"{2}\" height=\"{3}\" style=\"fill:{4}\" />";
 
     private readonly int Width;
     private readonly int Height;
@@ -19,15 +21,11 @@ public class SvgBuilder
     }
 
     public string Build() =>
-        string.Format(SVG_HEADER_TEMPLATE, Width, Height)
-            + Buffer
-            + SVG_FOOTER;
+        string.Format(SVG_HEADER_TEMPLATE, Width, Height) + Buffer + SVG_FOOTER;
 
     public SvgBuilder AddRectangle(int x, int y, int width, int height, string color)
     {
-        // Replace this line with your code
-        throw new NotImplementedException();
-
+        Buffer += string.Format(RECT_TEMPLATE, x, y, width, height, color);
         // Don't remove this!
         return this;
     }
