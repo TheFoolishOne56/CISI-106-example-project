dec5df12b1cc48b3e0a36554ea29819cb852b654
Aiden Whitacre <whitacrea@cougars.sf.edu>
2025-01-30 16:33:37 -0500

    allgood

--- SemesterProject/Program.cs
+++ SemesterProject/Program.cs
@@ -5,7 +5,7 @@ public static class Program
     public static void Main()
     {
         var svg = new SvgBuilder(500, 500)
-            .AddRectangle(100, 50, 150, 200, "#FF0000")
+            .AddRectangle(100, 50, 350, 200, "#FF0000")
             .AddRectangle(125, 400, 60, 30, "#00ff00")
             .AddRectangle(350, 400, 60, 30, "#00ff00")
             .Build();
